55dda67d96fef191b4f8b28494845c7239a789f6
André Schneider <hello@andreschneider.me>
2014-04-21 17:35:09 +0200

    initial commit.

---
+++ Example/ASCFlatUIColors/ASCFlatUIColors/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ASCFlatUIColors
+//
+//  Created by André Schneider on 21.04.14.
+//  Copyright (c) 2014 André Schneider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
