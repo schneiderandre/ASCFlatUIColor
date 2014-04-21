c4f6b83b48692fa9de783d1de58c906b691fe323
André Schneider <hello@andreschneider.me>
2014-04-21 18:26:29 +0200

    Add Flat UI Colors.

--- Classes/UIColor+ASCFlatUIColors.m
+++ Classes/UIColor+ASCFlatUIColors.m
@@ -10,4 +10,84 @@
 
 @implementation UIColor (ASCFlatUIColors)
 
++(UIColor *)turquoise {
+    return [UIColor colorWithRed:26/255.f green:188/255.f blue:156/255.f alpha:1.000];
+}
+
++(UIColor *)greenSea {
+    return [UIColor colorWithRed:22/255.f green:160/255.f blue:133/255.f alpha:1.000];
+}
+
++(UIColor *)emerland {
+    return [UIColor colorWithRed:46/255.f green:204/255.f blue:113/255.f alpha:1.000];
+}
+
++(UIColor *)nephritis {
+    return [UIColor colorWithRed:39/255.f green:174/255.f blue:96/255.f alpha:1.000];
+}
+
++(UIColor *)peterRiver {
+    return [UIColor colorWithRed:52/255.f green:152/255.f blue:219/255.f alpha:1.000];
+}
+
++(UIColor *)belizeHole {
+    return [UIColor colorWithRed:41/255.f green:128/255.f blue:185/255.f alpha:1.000];
+}
+
++(UIColor *)amethyst {
+    return [UIColor colorWithRed:155/255.f green:89/255.f blue:182/255.f alpha:1.000];
+}
+
++(UIColor *)wisteria {
+    return [UIColor colorWithRed:142/255.f green:86/255.f blue:173/255.f alpha:1.000];
+}
+
++(UIColor *)sunFlower {
+    return [UIColor colorWithRed:241/255.f green:196/255.f blue:15/255.f alpha:1.000];
+}
+
++(UIColor *)orange {
+    return [UIColor colorWithRed:243/255.f green:156/255.f blue:18/255.f alpha:1.000];
+}
+
++(UIColor *)carrot {
+    return [UIColor colorWithRed:230/255.f green:126/255.f blue:34/255.f alpha:1.000];
+}
+
++(UIColor *)pumpkin {
+    return [UIColor colorWithRed:211/255.f green:84/255.f blue:0/255.f alpha:1.000];
+}
+
++(UIColor *)alazarin {
+    return [UIColor colorWithRed:231/255.f green:76/255.f blue:60/255.f alpha:1.000];
+}
+
++(UIColor *)pomegranate {
+    return [UIColor colorWithRed:192/255.f green:57/255.f blue:43/255.f alpha:1.000];
+}
+
++(UIColor *)clouds {
+    return [UIColor colorWithRed:236/255.f green:240/255.f blue:241/255.f alpha:1.000];
+}
+
++(UIColor *)silver {
+    return [UIColor colorWithRed:189/255.f green:195/255.f blue:199/255.f alpha:1.000];
+}
+
++(UIColor *)concrete {
+    return [UIColor colorWithRed:149/255.f green:165/255.f blue:166/255.f alpha:1.000];
+}
+
++(UIColor *)asbestos {
+    return [UIColor colorWithRed:127/255.f green:140/255.f blue:141/255.f alpha:1.000];
+}
+
++(UIColor *)wetAsphalt {
+    return [UIColor colorWithRed:52/255.f green:73/255.f blue:94/255.f alpha:1.000];
+}
+
++(UIColor *)midnightBlue {
+    return [UIColor colorWithRed:44/255.f green:62/255.f blue:80/255.f alpha:1.000];
+}
+
 @end
---
+++ Example/ASCFlatUIColorsTests/ASCFlatUIColorsSpec.m
@@ -0,0 +1,137 @@
+//
+//  ASCFlatUIColorsSpec.m
+//  ASCFlatUIColors
+//
+//  Created by André Schneider on 21.04.14.
+//  Copyright (c) 2014 André Schneider. All rights reserved.
+//
+
+#import <Specta/Specta.h>
+#define EXP_SHORTHAND
+#import <Expecta/Expecta.h>
+#import <ASCFlatUIColors/UIColor+ASCFlatUIColors.h>
+
+SpecBegin(ASCFlatUIColors)
+
+describe(@"turqiose", ^{
+    it(@"returns a color", ^{
+        expect([UIColor turquoise]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"greenSea", ^{
+    it(@"returns a color", ^{
+        expect([UIColor greenSea]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"emerland", ^{
+    it(@"returns a color", ^{
+        expect([UIColor emerland]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"nephritis", ^{
+    it(@"returns a color", ^{
+        expect([UIColor nephritis]).to.beKindOf([UIColor class]);
+    });
+});
+
+
+describe(@"peterRiver", ^{
+    it(@"returns a color", ^{
+        expect([UIColor peterRiver]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"belizeHole", ^{
+    it(@"returns a color", ^{
+        expect([UIColor belizeHole]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"amethyst", ^{
+    it(@"returns a color", ^{
+        expect([UIColor amethyst]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"wisteria", ^{
+    it(@"returns a color", ^{
+        expect([UIColor wisteria]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"sunFlower", ^{
+    it(@"returns a color", ^{
+        expect([UIColor sunFlower]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"orange", ^{
+    it(@"returns a color", ^{
+        expect([UIColor orange]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"carrot", ^{
+    it(@"returns a color", ^{
+        expect([UIColor carrot]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"pumpkin", ^{
+    it(@"returns a color", ^{
+        expect([UIColor pumpkin]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"alazarin", ^{
+    it(@"returns a color", ^{
+        expect([UIColor alazarin]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"pomegranate", ^{
+    it(@"returns a color", ^{
+        expect([UIColor pomegranate]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"clouds", ^{
+    it(@"returns a color", ^{
+        expect([UIColor clouds]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"silver", ^{
+    it(@"returns a color", ^{
+        expect([UIColor silver]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"concrete", ^{
+    it(@"returns a color", ^{
+        expect([UIColor concrete]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"asbestos", ^{
+    it(@"returns a color", ^{
+        expect([UIColor turquoise]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"wetAsphalt", ^{
+    it(@"returns a color", ^{
+        expect([UIColor wetAsphalt]).to.beKindOf([UIColor class]);
+    });
+});
+
+describe(@"midnightBlue", ^{
+    it(@"returns a color", ^{
+        expect([UIColor midnightBlue]).to.beKindOf([UIColor class]);
+    });
+});
+
+SpecEnd
