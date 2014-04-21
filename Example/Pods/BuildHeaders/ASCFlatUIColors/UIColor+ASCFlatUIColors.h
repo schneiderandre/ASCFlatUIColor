d724c1e56da18ee6889bd5fe37a83a35ee3731bb
André Schneider <hello@andreschneider.me>
2014-04-21 18:11:29 +0200

    Move ASCFlatUIColors directory and install Pods.

---
+++ Example/Pods/BuildHeaders/ASCFlatUIColors/UIColor+ASCFlatUIColors.h
@@ -0,0 +1,1 @@
+../../../../Classes/UIColor+ASCFlatUIColors.h
---
+++ Example/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ASCFlatUIColors
+#define COCOAPODS_POD_AVAILABLE_ASCFlatUIColors
+#define COCOAPODS_VERSION_MAJOR_ASCFlatUIColors 0
+#define COCOAPODS_VERSION_MINOR_ASCFlatUIColors 1
+#define COCOAPODS_VERSION_PATCH_ASCFlatUIColors 0
+
